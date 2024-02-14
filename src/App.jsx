cdd93d098a9527200e13fa46eb5d23bcb9d8de4a
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-02-14 10:06:18 -0800

    getting the Router set up and added color scheme to the css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import NavBar from './components/NavBar';
+
 function App() {
-  return <h1>UX Portfolio</h1>
+  return (
+    <>
+      <h1>UX Portfolio</h1>
+      <NavBar />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>UX Case Studies</li>
+        <li>Contact</li>
+      </ul>
+    </>
+  );
+}
