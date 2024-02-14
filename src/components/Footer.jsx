8a5a9fb96d8ed584bf8cc3759cf8956bfc8f87ae
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-02-14 10:53:58 -0800

    created a footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <p>logo</p>
+        <p>linkedin</p>
+        <p>
+          <span className="scr-only">map-marker</span> Hesperia, CA
+        </p>
+      </footer>
+    </>
+  );
+}
