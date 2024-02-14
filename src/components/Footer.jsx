1abf4fc3325fd38d7a7e66a418b2d8658b35c937
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-02-14 13:16:25 -0800

    finished the nav and footer...I think

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,28 @@
+import logo from '../assets/logos/initialsLogoWhite.svg';
+import linkedIn from '../assets/images/linkedInLogoWhite.svg';
+
 export default function Footer() {
   return (
     <>
       <footer>
-        <p>logo</p>
-        <p>linkedin</p>
-        <p>
-          <span className="scr-only">map-marker</span> Hesperia, CA
-        </p>
+        <img src={logo} alt="" srcset="" className="FooterLogo" />
+        <div className="infoWrapper">
+          <a href="https://www.linkedin.com/in/sarahmlarsen/" target="_blank">
+            <img
+              src={linkedIn}
+              alt="Linkedin icon"
+              srcset=""
+              className="footerIcon"
+            />
+            https://www.linkedin.com/in/sarahmlarsen/
+          </a>
+          <p>
+            <span className="scr-only">map-marker</span> Hesperia, CA
+          </p>
+          <p>
+            <span className="scr-only">envelope</span> sarahlarsenux@gmail.com
+          </p>
+        </div>
       </footer>
     </>
   );
