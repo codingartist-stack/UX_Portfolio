dcb540053a98f52c1f941ef0c221a8ebb1385251
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-02-15 08:59:15 -0800

    trying to get the redirects to work so I can host on netlify

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,9 +1,15 @@
+import artMuseum from '../assets/images/ArtMuseum/artProject.png';
+
 export default function HomePage() {
   return (
     <>
-      <h1>UX Designer</h1>
-
-      <div>box 1</div>
+      <h1>Hi, I'm Sarah Larsen.</h1>
+      <p>
+        As a UX designer and artist, I employ a blend of creative and logical
+        approaches, allowing me to address both the emotional and practical
+        aspects of interface usability.
+      </p>
+      <img src={artMuseum} alt="art Museum project" className="heroImage" />
       <div>box 2</div>
       <div>box 3</div>
     </>
