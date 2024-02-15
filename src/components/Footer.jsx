6175c4866dedfd8870bbd70e2479b25e7f82d9b3
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-02-14 16:11:28 -0800

    fixed an image error working on making the menu responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,10 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <img src={logo} alt="" srcset="" className="FooterLogo" />
+        <img src={logo} alt="Sarah Larsen Logo" className="FooterLogo" />
         <div className="infoWrapper">
           <a href="https://www.linkedin.com/in/sarahmlarsen/" target="_blank">
-            <img
-              src={linkedIn}
-              alt="Linkedin icon"
-              srcset=""
-              className="footerIcon"
-            />
+            <img src={linkedIn} alt="Linkedin icon" className="footerIcon" />
             <span className="linkedInURL">
               https://www.linkedin.com/in/sarahmlarsen/
             </span>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,29 @@
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/logos/initialsLogoWhite.svg';
+import logo from '../assets/logos/initialsLogoPurple.svg';
+import { useState } from 'react';
 
 export default function NavBar() {
+  const [navToggle, setNavToggle] = useState(false);
+  const [menuContent, setMenuContent] = useState('menu');
+
+  const handleToggle = () => {
+    setNavToggle(!navToggle);
+
+    console.log(navToggle);
+  };
+
   return (
     <>
+      <button
+        className="scr-only mobileNavToggle"
+        aria-controls="navigation"
+        aria-expanded="false"
+        onClick={handleToggle}
+      >
+        {menuContent}
+      </button>
       <nav className="navigation">
-        <img src={logo} alt="" srcset="" className="navLogo" />
+        <img src={logo} className="navLogo" />
         <ul>
           <li>
             <NavLink to="/">Home</NavLink>
