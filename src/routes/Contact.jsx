66cd88c6d6a12532789d38f26f4b108bd1cd5127
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-02-15 13:20:19 -0800

    set the #root to grid so the footer can stick to the bottom of the page and the content.

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -4,24 +4,26 @@ export default function ContactPage() {
   return (
     <>
       <h1>Contact Me</h1>
-      <a
-        href="https://www.linkedin.com/in/sarahmlarsen/"
-        target="_blank"
-        className="flexLink"
-      >
-        <img
-          src={linkedIn}
-          alt="Linkedin icon"
-          className="footerIcon"
-          // width="24px"
-        />
-        <span className="linkedInURL">
-          https://www.linkedin.com/in/sarahmlarsen/
-        </span>
-      </a>
-      <p>
-        <span className="scr-only">envelope</span> sarahlarsenux@gmail.com
-      </p>
+      <div className="contactWrapper">
+        <a
+          href="https://www.linkedin.com/in/sarahmlarsen/"
+          target="_blank"
+          className="flexLink"
+        >
+          <img
+            src={linkedIn}
+            alt="Linkedin icon"
+            className="footerIcon"
+            // width="24px"
+          />
+          <span className="linkedInURL">
+            https://www.linkedin.com/in/sarahmlarsen/
+          </span>
+        </a>
+        <p>
+          <span className="scr-only">envelope</span> sarahlarsenux@gmail.com
+        </p>
+      </div>
     </>
   );
 }
