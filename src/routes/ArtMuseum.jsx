24d5a6f597fec8cc47e85b3a76faf55bcaddae7a
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-02-15 13:42:37 -0800

    got the iframe to ge responsive. it was easier than I thought it would be. it's working

--- src/routes/ArtMuseum.jsx
+++ src/routes/ArtMuseum.jsx
@@ -2,37 +2,44 @@ export default function ArtMuseumPage() {
   return (
     <>
       <h1>Responsive Art Museum</h1>
-      <p>
-        To create a responsive Art Museum website I looked into users that enjoy
-        looking at art. I made the assumption that if users were looking at an
-        art museum&apos;s website or app they would like to see the art in
-        person. In reality users would like to know what to expect to see and if
-        the Museum offers a place to study, learn, and create art.
-      </p>
-      <a
-        href="https://www.figma.com/file/Qyh7Pb9K30MDvWM1Rd5A1H/Art-Museum?type=design&node-id=50%3A2338&mode=design&t=gJLxmQItryFdRidO-1"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Figma High Fidelity Mockups
-      </a>
-      <a
-        href="https://www.figma.com/proto/Qyh7Pb9K30MDvWM1Rd5A1H/Art-Museum?node-id=78-2867&starting-point-node-id=78%3A2867&mode=design&t=A3uOJju3LXfvRfji-1"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Mobile High Fidelity Prototype
-      </a>
-
-      <iframe
-        src="https://docs.google.com/presentation/d/e/2PACX-1vSduZ0peqFpbkKdeVscTincCMf9WzRP4LqTXfx4CtaRKEBUCSVx_vDcbXdbVzLN4L_Sm7LZlF7nFipG/embed?start=false&loop=false&delayms=3000"
-        frameborder="0"
-        width="960"
-        height="569"
-        allowfullscreen="true"
-        mozallowfullscreen="true"
-        webkitallowfullscreen="true"
-      ></iframe>
+      <div className="contentWrapper">
+        <p>
+          To create a responsive Art Museum website I looked into users that
+          enjoy looking at art. I made the assumption that if users were looking
+          at an art museum&apos;s website or app they would like to see the art
+          in person. In reality users would like to know what to expect to see
+          and if the Museum offers a place to study, learn, and create art.
+        </p>
+        <p>
+          <a
+            href="https://www.figma.com/file/Qyh7Pb9K30MDvWM1Rd5A1H/Art-Museum?type=design&node-id=50%3A2338&mode=design&t=gJLxmQItryFdRidO-1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Figma High Fidelity Mockups
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://www.figma.com/proto/Qyh7Pb9K30MDvWM1Rd5A1H/Art-Museum?node-id=78-2867&starting-point-node-id=78%3A2867&mode=design&t=A3uOJju3LXfvRfji-1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mobile High Fidelity Prototype
+          </a>
+        </p>
+        <div className="iframeWrapper">
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vSduZ0peqFpbkKdeVscTincCMf9WzRP4LqTXfx4CtaRKEBUCSVx_vDcbXdbVzLN4L_Sm7LZlF7nFipG/embed?start=false&loop=false&delayms=3000"
+            frameborder="0"
+            // width="960"
+            // height="569"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>
+        </div>
+      </div>
     </>
   );
 }
