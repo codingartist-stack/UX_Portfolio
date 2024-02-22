7568d4b1b2aa641994d0c52989d611a38ee925e5
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-02-22 10:41:15 -0800

    I have to redesign the case study page. I have to sketch it out first.

--- src/routes/ArtMuseum.jsx
+++ src/routes/ArtMuseum.jsx
@@ -14,42 +14,13 @@ export default function ArtMuseumPage() {
         </p>
         <p>
           <a
-            href="https://www.figma.com/file/Qyh7Pb9K30MDvWM1Rd5A1H/Art-Museum?type=design&node-id=50%3A2338&mode=design&t=gJLxmQItryFdRidO-1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Figma High Fidelity Mockups
-          </a>
-        </p>
-        <p>
-          <a
-            href="https://www.figma.com/proto/Qyh7Pb9K30MDvWM1Rd5A1H/Art-Museum?node-id=78-2867&starting-point-node-id=78%3A2867&mode=design&t=A3uOJju3LXfvRfji-1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Mobile High Fidelity Prototype
-          </a>
-        </p>
-        <p>
-          <a
             href="https://docs.google.com/presentation/d/18YKe5o1r_ceKvSgS5rVZzwj4A_vJFkZVz66f-tQ5cjY/edit?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Case study
+            Case study Slides
           </a>
         </p>
-        <div className="iframeWrapper">
-          <iframe
-            src="https://docs.google.com/presentation/d/e/2PACX-1vSduZ0peqFpbkKdeVscTincCMf9WzRP4LqTXfx4CtaRKEBUCSVx_vDcbXdbVzLN4L_Sm7LZlF7nFipG/embed?start=false&loop=false&delayms=3000"
-            frameborder="0"
-            // width="960"
-            // height="569"
-            allowfullscreen="true"
-            mozallowfullscreen="true"
-            webkitallowfullscreen="true"
-          ></iframe>
-        </div>
       </div>
     </>
   );
