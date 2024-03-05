360bd32899d35305ab6d59b4c36c1f03022024b8
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-03-05 13:39:27 -0800

    edited header on about page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
     <>
-      <h1>About</h1>
+      <h1>About Me</h1>
       <p>
         As a UX designer and artist, I employ a blend of creative and logical
         approaches, allowing me to address both the emotional and practical
