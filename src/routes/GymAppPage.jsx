b4631cb2b207f40567e52dd6759842f424944d7b
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-03-05 13:37:11 -0800

    added gif to the gym page

--- src/routes/GymAppPage.jsx
+++ src/routes/GymAppPage.jsx
@@ -1,18 +1,40 @@
+import phoneGif from '../assets/images/GymApp/GymPrototype.gif';
+
 export default function GymAppPage() {
   return (
     <>
-      <h1>Redesign: Local Recreation Center</h1>
       <div className="contentWrapper">
-        <p>
-          My first Figma Project. This application is designed for the Orem
-          Recreation Center, a local gym I frequented during my time in Orem,
-          Utah. Intended to enhance your recreation center experience, the app
-          addresses the challenges posed by the current website, which often
-          leads to confusion and frustration for users. Primarily catering to
-          mothers, the app simplifies the process of signing up their children
-          for various classes, including dance, swim, and sports, offered by the
-          recreation center.
-        </p>
+        <h1>Redesign: Local Recreation Center</h1>
+        <div className="projectWrapper">
+          <h2>Project:</h2>
+          <p>
+            My first Figma Project. This application is designed for the Orem
+            Recreation Center, a local gym I frequented during my time in Orem,
+            Utah. Intended to enhance your recreation center experience, the app
+            addresses the challenges posed by the current website, which often
+            leads to confusion and frustration for users. Primarily catering to
+            mothers, the app simplifies the process of signing up their children
+            for various classes, including dance, swim, and sports, offered by
+            the recreation center.
+          </p>
+          <img src={phoneGif} alt="Final prototype gif" className="mobileGif" />
+        </div>
+        <hr />
+        <div className="projectDetails">
+          <p>Created by: Sarah Larsen</p>
+          <p>Role: brand identity, lead UX designer and researcher</p>
+          <p>Project Duration: April 2022 to Aug 2022</p>
+          <p>
+            <a
+              href="https://docs.google.com/presentation/d/18YKe5o1r_ceKvSgS5rVZzwj4A_vJFkZVz66f-tQ5cjY/edit?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Case Study Slide Presentation
+            </a>
+          </p>
+        </div>
+        <hr />
         <p>
           <a
             href="https://www.figma.com/file/YH0F1UFBFNj0xZzKAW6UJL/Gym-App?type=design&node-id=125%3A1361&mode=design&t=HhRsOedWQJIQOYeJ-1"
