7bd55a6c4610b2db57c1ed12375ad8b98b4b4cb2
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-03-05 19:33:13 -0800

    cleaned up the spacing for the about page.

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,31 +1,34 @@
 export default function AboutPage() {
   return (
-    <>
+    <div className="contentWrapper">
       <h1>About Me</h1>
-      <p>
-        As a UX designer and artist, I employ a blend of creative and logical
-        approaches, allowing me to address both the emotional and practical
-        aspects of interface usability. Having engaged directly with users
-        during my tenure at the second-largest hosting provider and education
-        technology company in technical support roles, I developed a profound
-        interest in not just empathizing with users' concerns but actively
-        resolving them through the lens of UX/UI design.
-      </p>
-      <p>
-        Over the last five years, my primary focus has been raising my daughter.
-        Now that she is starting school, I am eager to re-enter the workforce.
-        Two years ago, I embarked on my UX journey and gained valuable insights.
-        I dedicated a year to mastering HTML, CSS, and React. Upon returning to
-        UX design, I saw how important understanding front-end development was.
-        This knowledge of element and layout limitations has greatly influenced
-        my design process. Combining both skill sets, I successfully built my
-        portfolio site – the one you are currently exploring!
-      </p>
-      <p>
-        Thank you for taking the time to learn more about me, Sarah Larsen. I
-        appreciate your interest and look forward to future endeavors and
-        collaborations.
-      </p>
-    </>
+      <div className="aboutPara">
+        <p>
+          As a UX designer and artist, I employ a blend of creative and logical
+          approaches, allowing me to address both the emotional and practical
+          aspects of interface usability. Having engaged directly with users
+          during my tenure at the second-largest hosting provider and education
+          technology company in technical support roles, I developed a profound
+          interest in not just empathizing with users' concerns but actively
+          resolving them through the lens of UX/UI design.
+        </p>
+        <p>
+          Over the last five years, my primary focus has been raising my
+          daughter. Now that she is starting school, I am eager to re-enter the
+          workforce. Two years ago, I embarked on my UX journey and gained
+          valuable insights. I dedicated a year to mastering HTML, CSS, and
+          React. Upon returning to UX design, I saw how important understanding
+          front-end development was. This knowledge of element and layout
+          limitations has greatly influenced my design process. Combining both
+          skill sets, I successfully built my portfolio site – the one you are
+          currently exploring!
+        </p>
+        <p>
+          Thank you for taking the time to learn more about me, Sarah Larsen. I
+          appreciate your interest and look forward to future endeavors and
+          collaborations.
+        </p>
+      </div>
+    </div>
   );
 }
