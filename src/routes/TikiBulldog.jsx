670d58cee5a4ff741e57833e35b2f8a6ad3ad52f
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-03-21 15:45:06 -0700

    started writing the teacher appreciation week page

--- src/routes/TikiBulldog.jsx
+++ src/routes/TikiBulldog.jsx
@@ -1,7 +1,13 @@
 export default function TikiBulldog() {
   return (
     <>
-      <h1>Teacher Appreciation</h1>
+      <h1>Teacher Appreciation Week</h1>
+      <p>
+        This year, my daughter's school teacher appreciation week will have a
+        Luau theme. I offered to create a image they could use for the week. I
+        wanted to incorporate the school's mascot, a bulldog, into the design.
+        Even before I started sketching I new I wanted a Tiki Bulldog.
+      </p>
     </>
   );
 }
